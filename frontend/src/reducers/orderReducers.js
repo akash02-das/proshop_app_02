148534363fca015d4bc5e734bcbab3e121344249
akash das <aku.das02@gmail.com>
2021-03-19 22:48:40 +0600

    show orders on profile

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,10 +5,13 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
-  ORDER_PAY_REQUEST,
-  ORDER_PAY_SUCCESS,
-  ORDER_PAY_FAIL,
-  ORDER_PAY_RESET,
+  // ORDER_PAY_REQUEST,
+  // ORDER_PAY_SUCCESS,
+  // ORDER_PAY_FAIL,
+  // ORDER_PAY_RESET,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -64,28 +67,52 @@ export const orderDetailsReducer = (
   }
 };
 
-export const orderPayReducer = (state = {}, action) => {
+// export const orderPayReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case ORDER_PAY_REQUEST:
+//       return {
+//         ...state,
+//         loading: true,
+//       };
+
+//     case ORDER_PAY_SUCCESS:
+//       return {
+//         loading: false,
+//         success: true,
+//       };
+
+//     case ORDER_PAY_FAIL:
+//       return {
+//         loading: false,
+//         error: action.payload,
+//       };
+
+//     case ORDER_PAY_RESET:
+//       return {};
+
+//     default:
+//       return state;
+//   }
+// };
+
+export const orderListMyReducer = (state = { orders: [] }, action) => {
   switch (action.type) {
-    case ORDER_PAY_REQUEST:
+    case ORDER_LIST_MY_REQUEST:
       return {
-        ...state,
         loading: true,
       };
 
-    case ORDER_PAY_SUCCESS:
+    case ORDER_LIST_MY_SUCCESS:
       return {
         loading: false,
-        success: true,
+        orders: action.payload,
       };
 
-    case ORDER_PAY_FAIL:
+    case ORDER_LIST_MY_FAIL:
       return {
         loading: false,
         error: action.payload,
       };
-
-    case ORDER_PAY_RESET:
-      return {};
 
     default:
       return state;
