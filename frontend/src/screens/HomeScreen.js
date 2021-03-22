28facccc3d8402c54fbb84e597cfe02001e59615
akash das <aku.das02@gmail.com>
2021-03-22 20:27:54 +0600

    top products carousel

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = ({ match }) => {
@@ -23,6 +24,8 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
+      {!keyword && <ProductCarousel />}
+
       <h1>Latest Products</h1>
 
       {loading ? (
